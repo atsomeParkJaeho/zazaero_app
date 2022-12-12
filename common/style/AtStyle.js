1627b3fe140c8a3a61cc89709fe495c2e6ab99ae
parkjaeho <woghsla85@naver.com>
2022-12-12 16:18:24 +0900

    공통 css 추가
    1. 버튼
    2. 서브레이아웃
    3. 색상, 배경

--- common/style/AtStyle.js
+++ common/style/AtStyle.js
@@ -1,10 +1,78 @@
-export const container = {
+export const container = {      // 하단 간격 설정
     padding:16,
+    paddingBottom:100,
 }
-export const bg_white = {
-    backgroundColor:"#fff",
-    height:"100%",
-}
+// export const bg_white = {
+//     backgroundColor:"#fff",
+//     height:"100%",
+// }
+
+// 폰트사이즈 설정
+export const h14 = {fontSize:14, lineHeight: 1.5,}
+export const h15 = {fontSize:15, lineHeight: 1.5,}
+export const h16 = {fontSize:16, lineHeight: 1.5,}
+export const h17 = {fontSize:17, lineHeight: 1.5,}
+export const h18 = {fontSize:18, lineHeight: 1.5,}
+export const h19 = {fontSize:19, lineHeight: 1.5,}
+export const h20 = {fontSize:20, lineHeight: 1.5,}
+export const h21 = {fontSize:21, lineHeight: 1.5,}
+export const h22 = {fontSize:22, lineHeight: 1.5,}
+
+// 폼 이동 버튼 설정
+export const btn_box  = {position:"absolute", left:0, bottom:0, width:"100%", backgroundColor: "#B1B2C3"}
+
+
+// 색상 클래스
+export const text_primary = {textColor:"#3D40E0",}
+export const text_secondary = {textColor:"#15db95",}
+export const text_success = {textColor:"#5cc9a7",}
+export const text_info = {textColor:"#50b5ff",}
+export const text_warning = {textColor:"#FFBE3D",}
+export const text_danger = {textColor:"#F24A64",}
+export const text_white = {textColor:"#fff",}
+export const text_dark = {textColor:"#08052f",}
+export const text_black = {textColor:"#000",}
+export const text_gray = {textColor:"#B1B2C3",}
+export const text_light = {textColor:"#F9F9FB",}
+
+export const bg_primary = {backgroundColor:"#3D40E0",}
+export const bg_secondary = {backgroundColor:"#15db95",}
+export const bg_success = {backgroundColor:"#5cc9a7",}
+export const bg_info = {backgroundColor:"#50b5ff",}
+export const bg_warning = {backgroundColor:"#FFBE3D",}
+export const bg_danger = {backgroundColor:"#F24A64",}
+export const bg_white = {backgroundColor:"#fff",}
+export const bg_dark = {backgroundColor:"#08052f",}
+export const bg_black = {backgroundColor:"#000",}
+export const bg_gray = {backgroundColor:"#B1B2C3",}
+export const bg_light = {backgroundColor:"#F9F9FB",}
+
+// 버튼 설정
+export const btn_primary = {backgroundColor:"#3D40E0", textColor:"#FFF",}
+export const btn_secondary = {backgroundColor:"#15db95", textColor:"#FFF",}
+export const btn_success = {backgroundColor:"#5cc9a7", textColor:"#FFF",}
+export const btn_info = {backgroundColor:"#50b5ff", textColor:"#FFF",}
+export const btn_warning = {backgroundColor:"#FFBE3D", textColor:"#FFF",}
+export const btn_danger = {backgroundColor:"#F24A64", textColor:"#FFF",}
+export const btn_white = {backgroundColor:"#fff", textColor:"#FFF",}
+export const btn_dark = {backgroundColor:"#08052f", textColor:"#FFF",}
+export const btn_black = {backgroundColor:"#000", textColor:"#FFF",}
+export const btn_gray = {backgroundColor:"#B1B2C3", textColor:"#3E3E4C",}
+export const btn_light = {backgroundColor:"#F9F9FB", textColor:"#3E3E4C",}
+
+export const btn_outline_primary = {borderColor:"#3D40E0", borderWidth:1, textColor:"#3D40E0",}
+export const btn_outline_secondary = {borderColor:"#15db95", borderWidth:1, textColor:"#15db95",}
+export const btn_outline_success = {borderColor:"#5cc9a7", borderWidth:1, textColor:"#5cc9a7",}
+export const btn_outline_info = {borderColor:"#50b5ff", borderWidth:1, textColor:"#50b5ff",}
+export const btn_outline_warning = {borderColor:"#FFBE3D", borderWidth:1, textColor:"#FFBE3D",}
+export const btn_outline_danger = {borderColor:"#F24A64", borderWidth:1, textColor:"#F24A64",}
+export const btn_outline_white = {borderColor:"#fff", borderWidth:1, textColor:"#fff",}
+export const btn_outline_dark = {borderColor:"#08052f", borderWidth:1, textColor:"#08052f",}
+export const btn_outline_black = {borderColor:"#000", borderWidth:1, textColor:"#000",}
+export const btn_outline_gray = {borderColor:"#B1B2C3", borderWidth:1, textColor:"#B1B2C3",}
+export const btn_outline_light = {borderColor:"#F9F9FB", borderWidth:1, textColor:"#F9F9FB",}
+
+
 export const flex = {
     flexDirection:"row",
     alignItems:"center",
