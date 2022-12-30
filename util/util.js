a03d212dad7916d6211cd9d7e71a01d8122441d6
parkjaeho <woghsla85@naver.com>
2022-12-30 15:40:46 +0900

    회원가입 수정

--- util/util.js
+++ util/util.js
@@ -367,71 +367,71 @@ export const bankAccount = ["자재로(이정완)_국민은행 1234-23-5968714"]
 
 export const AddrMatch = [
     {
-        key          :"서울특별시",
+        label          :"서울특별시",
         value        :"서울",
     },
     {
-        key          :"부산광역시",
+        label          :"부산광역시",
         value        :"부산",
     },
     {
-        key          :"울산광역시",
+        label          :"울산광역시",
         value        :"울산",
     },
     {
-        key          :"대구광역시",
+        label          :"대구광역시",
         value        :"대구",
     },
     {
-        key          :"인천광역시",
+        label          :"인천광역시",
         value        :"인천",
     },
     {
-        key          :"대전광역시",
+        label          :"대전광역시",
         value        :"대전",
     },
     {
-        key          :"광주광역시",
+        label          :"광주광역시",
         value        :"광주",
     },
     {
-        key          :"세종특별자치시",
+        label          :"세종특별자치시",
         value        :"세종",
     },
     {
-        key          :"경기도",
+        label          :"경기도",
         value        :"경기",
     },
     {
-        key          :"강원도",
+        label          :"강원도",
         value        :"강원",
     },
     {
-        key          :"충청남도",
+        label          :"충청남도",
         value        :"충남",
     },
     {
-        key          :"충청북도",
+        label          :"충청북도",
         value        :"충북",
     },
     {
-        key          :"경상남도",
+        label          :"경상남도",
         value        :"경남",
     },
     {
-        key          :"경상북도",
+        label          :"경상북도",
         value        :"경북",
     },
     {
-        key          :"전라남도",
+        label          :"전라남도",
         value        :"전남",
     },
     {
-        key          :"전라북도",
+        label          :"전라북도",
         value        :"전북",
     },
     {
-        key          :"제주특별자치도",
+        label          :"제주특별자치도",
         value        :"제주",
     },
 ];
