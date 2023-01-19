9c66b3c7c529ec389b3a8db4f726b677b999d968
master79 <jms566690@gmail.com>
2023-01-19 18:06:47 +0900

    고객센터 페이지 스타일수정 전화연결 기능 추가

--- pages/Cscenter.js
+++ pages/Cscenter.js
@@ -1,142 +1,62 @@
 import React from 'react'
-import {View, Text, StyleSheet, Image, TouchableOpacity, ScrollView} from 'react-native'
+import {View, Text, StyleSheet, Image, TouchableOpacity, ScrollView,Linking} from 'react-native'
 import logo from "../assets/img/top_logo.png";
 import Icon from "react-native-vector-icons/AntDesign";
+import {bg_white, text_primary} from "../common/style/AtStyle";
+import ArrowRight from "../icons/arrow_r.svg";
 
 
 export default function Cscenter({navigation}) {
 
-    console.log('고객센터');
+    // console.log('고객센터');
 
 
     return (
-        <ScrollView style={styles.container}>
-            <View style={styles.mypageList}>
-                <TouchableOpacity onPress={()=>{navigation.navigate('자주묻는질문')}}>
-                    <View style={styles.mypageListItem}>
-                        <View style={styles.mypageListItemTitle}>
-                            <Text style={styles.mypageList_name}>자주묻는질문</Text>
+        <>
+            <ScrollView style={[bg_white]}>
+                <View style={styles.mypageList}>
+                    <TouchableOpacity onPress={()=>{navigation.navigate('자주묻는질문')}}>
+                        <View style={styles.mypageListItem}>
+                            <View style={styles.mypageListItemTitle}>
+                                <Text style={styles.mypageList_name}>자주묻는질문</Text>
+                            </View>
+                            <View style={styles.mypageListItemIcon}>
+                                <Text style={styles.mypageList_name}> <ArrowRight width={11} height={18} /> </Text>
+                            </View>
                         </View>
-                        <View style={styles.mypageListItemIcon}>
-                            <Text style={styles.mypageList_name}> > </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => {navigation.navigate('1:1문의작성')}}>
+                        <View style={styles.mypageListItem}>
+                            <View style={styles.mypageListItemTitle}>
+                                <Text style={styles.mypageList_name}>1:1문의</Text>
+                            </View>
+                            <View style={styles.mypageListItemIcon}>
+                                <Text style={styles.mypageList_name}> <ArrowRight width={11} height={18} /> </Text>
+                            </View>
                         </View>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => {navigation.navigate('1:1문의작성')}}>
-                    <View style={styles.mypageListItem}>
-                        <View style={styles.mypageListItemTitle}>
-                            <Text style={styles.mypageList_name}>1:1문의</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => {Linking.openURL('tel:1666-7099');}}>
+                        <View style={styles.mypageListItem}>
+                            <View style={styles.mypageListItemTitle}>
+                                <Text style={styles.mypageList_name}>고객센터</Text>
+                            </View>
+                            <View style={styles.mypageListItemIcon}>
+                                {/*<Icon name="chevron-forward-outline" size={25} color="#000" />*/}
+                                <Text style={[styles.mypageList_name,text_primary]}> 1666-7099 </Text>
+                            </View>
                         </View>
-                        <View style={styles.mypageListItemIcon}>
-                            <Text style={styles.mypageList_name}> > </Text>
-                        </View>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate('고객센터')}>
-                    <View style={styles.mypageListItem}>
-                        <View style={styles.mypageListItemTitle}>
-                            <Text style={styles.mypageList_name}>고객센터</Text>
-                        </View>
-                        <View style={styles.mypageListItemIcon}>
-                            {/*<Icon name="chevron-forward-outline" size={25} color="#000" />*/}
-                            <Text style={styles.mypageList_name}> > </Text>
-                        </View>
-                    </View>
-                </TouchableOpacity>
-            </View>
-
-        </ScrollView>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </>
     )
 }
 
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#fff",
-    },
-    top_inner: {
-        marginTop: 50,
-        paddingLeft: 12,
-        paddingRight: 12,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-    },
-    me_10: {
-        marginRight: 8,
-    },
-    top_innertwo: {
-        flexDirection: "row",
-        justifyContent: "space-around",
-    },
-    toptitle: {
-        fontSize: Platform.OS === 'ios' ? 18 : 17,
-        fontWeight: "700",
-        color: "#222",
-    },
-    mypageinfo: {
-        paddingLeft: 12,
-        paddingRight: 12,
-        padding: 16,
-        borderBottomWidth: 8,
-        borderColor: '#ededf1',
-
-    },
     mypageList_name: {
-        fontSize: Platform.OS === 'ios' ? 20 : 19,
+        fontSize: Platform.OS === 'ios' ? 17 : 16,
         color: "#08052f",
-        fontFamily: "Amatic-Bold",
-        fontWeight: "700",
-    },
-    id: {
-        marginStart: 6,
-        fontSize: 17,
-        color: "#08052f",
-        fontWeight: "700",
-    },
-    title: {
-        fontSize: Platform.OS === 'ios' ? 30 : 29,
-        fontWeight: "700",
-        color: "#fff",
-        paddingLeft: 30,
-        paddingTop: 100,
-        paddingRight: 30
-    },
-    textContainer: {
-        backgroundColor: "#fff",
-        marginTop: 12,
-        borderRadius: 30,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    aboutImage: {
-        width: 150,
-        height: 150,
-        borderRadius: 30
-    },
-    desc01: {
-        textAlign: "center",
-        fontSize: Platform.OS === 'ios' ? 20 : 19,
-        fontWeight: "700",
-        paddingLeft: 22,
-        paddingRight: 22
-
-    },
-    desc02: {
-        textAlign: "center",
-        fontSize: Platform.OS === 'ios' ? 15 : 14,
-        fontWeight: "700",
-        padding: 22
-    },
-    button: {
-        backgroundColor: "orange",
-        padding: 20,
-        borderRadius: 15
-    },
-    buttonText: {
-        color: "#fff",
-        fontSize: Platform.OS === 'ios' ? 15 : 14,
-        fontWeight: "700"
     },
     mypageList: {
         marginTop: 30,
